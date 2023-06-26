8eb6877c7d3bb0a8cb11bb5eed1a3427a059d3a0
danil <danil22344bel@gmail.com>
2023-06-26 22:19:26 +0300

    Add MessageService

---
+++ src/main/java/dev/danilbel/schedule/bot/service/MessageService.java
@@ -0,0 +1,18 @@
+package dev.danilbel.schedule.bot.service;
+
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.ParseMode;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Service
+public class MessageService {
+
+    public SendMessage makeSendMessageWithText(Update update, String text) {
+        var sendMessage = new SendMessage();
+        sendMessage.setParseMode(ParseMode.HTML);
+        sendMessage.setChatId(update.getMessage().getChatId());
+        sendMessage.setText(text);
+        return sendMessage;
+    }
+}
