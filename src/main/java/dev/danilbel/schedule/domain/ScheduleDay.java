4bd0ea1985d5e85f99b46eac770ee84c6047891b
danil <danil22344bel@gmail.com>
2023-06-29 21:42:26 +0300

    Implementing the method toString for ScheduleDay and SchedulePair

--- src/main/java/dev/danilbel/schedule/domain/ScheduleDay.java
+++ src/main/java/dev/danilbel/schedule/domain/ScheduleDay.java
@@ -21,4 +21,38 @@ public class ScheduleDay {
     WeekDay day;
 
     List<SchedulePair> pairs;
+
+    @Override
+    public String toString() {
+        var stringBuilderDay = new StringBuilder();
+
+        var count = 0;
+        for (var timeTable : TimeTable.values()) {
+
+            var countPair = 0;
+            var stringBuilderPairs = new StringBuilder();
+            for (var pair : pairs) {
+                if (pair.getTime() == timeTable) {
+                    stringBuilderPairs.append('\n').append(pair);
+
+                    countPair++;
+                }
+            }
+
+            if (countPair > 0) {
+
+                if (count > 0) stringBuilderDay.append('\n');
+
+                stringBuilderDay.append(timeTable).append(stringBuilderPairs);
+
+                count++;
+            }
+        }
+
+        if (count == 0) {
+            stringBuilderDay.append("<i>Пар немає</i>");
+        }
+
+        return stringBuilderDay.toString();
+    }
 }
